fecfaaa47829d7fe077fa1be2cf240b446bfefe0
Mirjalol Mamaziyatov <mirjaloltv@gmail.com>
2025-03-05 12:45:57 +0500

    fix

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,9 @@ Route::middleware('api')->prefix('api')->group(function () {
     Route::get('/province-list', [CandidateController::class, 'getProvince']);
     Route::get('/region-list', [CandidateController::class, 'getRegion']);
     Route::get('/village-list', [CandidateController::class, 'getVillage']);
+});
+
+Route::middleware('redis-auth')->prefix('api')->group(function () {
     Route::get('get-candidate',[CandidateController::class, 'getCandidates']);
     Route::get('get-candidate/{Candidate}',[CandidateController::class, 'getCandidateDetails']);
 });
